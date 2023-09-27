d0ce690282feff5828440c061880e579fcf5a25b
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-09-27 13:29:55 +0300

    main  file

---
+++ src/main/java/edchey/dev/movieapispringboot/MovieApiSpringbootApplication.java
@@ -0,0 +1,12 @@
+package edchey.dev.movieapispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieApiSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieApiSpringbootApplication.class, args);
+    }
+}

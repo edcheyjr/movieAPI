694d6bdfde40e03773eeaa009fc7385bb37bc189
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-09-27 13:30:05 +0300

    main test file

---
+++ src/test/java/edchey/dev/movieapispringboot/MovieApiSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package edchey.dev.movieapispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieApiSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

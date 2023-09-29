4c043bd072de7ebb55548ea04afa90bd0563ba80
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-09-29 13:52:22 +0300

    Add Table definitions
    Signed-off-by: Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>

--- src/main/java/edchey/dev/movieapispringboot/models/Movie.java
+++ src/main/java/edchey/dev/movieapispringboot/models/Movie.java
@@ -15,6 +15,18 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Movie {
+    //    TABLE DEFINITIONS
+    public static final String ID = "id";
+    public static final String IMDB_ID = "imdbId";
+    public static final String TITLE = "title";
+    public static final String RELEASE_DATE = "releaseDate";
+    public static final String TRAILER_LINK = "trailerLink";
+    public static final String POSTER = "poster";
+    public static final String GENRES = "genres";
+    public static final String BACKDROPS = "backdrops";
+    public static final String REVIEWS_IDS = "reviewIds";
+
+    //  Table blueprint
     @Id
     private ObjectId id;
     private String imdbId;

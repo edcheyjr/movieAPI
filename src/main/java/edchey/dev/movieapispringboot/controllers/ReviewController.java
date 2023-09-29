bf7ccd7108ef532a7f26f256b02ecde8722192bd
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-09-29 16:00:51 +0300

    todo
    Signed-off-by: Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>

--- src/main/java/edchey/dev/movieapispringboot/controllers/ReviewController.java
+++ src/main/java/edchey/dev/movieapispringboot/controllers/ReviewController.java
@@ -15,6 +15,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/v1/reviews")
 public class ReviewController {
+    // TODO: Finish Crud
     @Autowired
     private ReviewService reviewService;
 
